3b66f06be296d5acb09bdcf0e22181b9516cb1c7
Amxx79 <e.siwanov@gmail.com>
2024-05-23 00:40:55 +0300

    CRUD Operations

---
+++ 02. CRUD Operations/CRUD Operations.sql
@@ -0,0 +1,50 @@
+--01.
+--Install three databases and looking in it.
+
+--02.
+SELECT * FROM Departments
+
+--03.
+SELECT [Name] FROM Departments
+
+--04.
+SELECT FirstName, LastName, Salary FROM Employees
+
+--05.
+SELECT FirstName, MiddleName, LastName FROM Employees
+
+--06.
+SELECT 
+CONCAT(FirstName,'.', LastName,'@softuni.bg') AS
+[Full Email Address]
+  FROM Employees
+
+--07.
+SELECT DISTINCT Salary
+FROM Employees
+
+--08.
+SELECT * FROM Employees
+ WHERE JobTitle = 'Sales Representative'
+
+--09.
+SELECT FirstName, LastName, JobTitle
+ FROM Employees
+ WHERE Salary BETWEEN 20000 AND 30000
+
+--10.
+ SELECT
+ CONCAT_WS(' ', FirstName, MiddleName, LastName)
+		AS [Full Name]
+	  FROM Employees
+	 WHERE Salary = 25000 OR Salary = 14000 OR Salary = 12500 OR Salary = 23600
+
+--11.
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE ManagerID IS NULL
+
+--12.
+SELECT FirstName, LastName, Salary
+  FROM Employees
+  WHERE Salary > 50000
