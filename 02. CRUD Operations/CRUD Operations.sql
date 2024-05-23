e01aff08720e71b0e6b7549f3f62312b52035485
Amxx79 <e.siwanov@gmail.com>
2024-05-23 23:59:10 +0300

    CRUD Operations

--- 02. CRUD Operations/CRUD Operations.sql
+++ 02. CRUD Operations/CRUD Operations.sql
@@ -48,3 +48,102 @@ SELECT FirstName, LastName
 SELECT FirstName, LastName, Salary
   FROM Employees
   WHERE Salary > 50000
+  ORDER BY Salary DESC
+
+--13.
+SELECT TOP(5) FirstName, LastName
+  FROM Employees
+  ORDER BY Salary DESC
+
+--14.
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE DepartmentID != 4
+
+--15.
+SELECT * 
+  FROM Employees
+  ORDER BY Salary DESC,
+		   FirstName,
+		   LastName DESC,
+		   MiddleName
+
+--16.
+CREATE VIEW V_EmployeesSalaries AS
+(
+	SELECT FirstName, LastName, Salary
+	  FROM Employees
+)
+
+--How to view 'VIEW'
+SELECT * FROM V_EmployeesSalaries
+
+--17.
+CREATE VIEW V_EmployeeNameJobTitle AS
+(
+	SELECT
+	CONCAT(FirstName, ' ', COALESCE(MiddleName, ''), ' ', LastName) AS 'Full Name', JobTitle
+	  FROM Employees
+)
+
+SELECT * FROM V_EmployeeNameJobTitle
+
+--18.
+SELECT DISTINCT JobTitle 
+  FROM Employees
+
+--19.
+SELECT TOP(10) * 
+  FROM Projects
+  ORDER BY StartDate,
+			    [Name]
+
+--20.
+SELECT TOP(7) FirstName, LastName, HireDate
+  FROM Employees
+  ORDER BY HireDate DESC
+
+--21.
+
+--Transaction in function where update some info and revert it when we call 'RollBack'
+
+BEGIN TRANSACTION
+UPDATE Employees
+   SET Salary = Salary * 1.12
+WHERE DepartmentID IN(1, 2, 4, 11)
+SELECT Salary FROM Employees
+ROLLBACK TRANSACTION
+
+SELECT SALARY FROM Employees
+WHERE DepartmentID = 1
+
+--II - Part Two (Use Geography database)
+
+--22.
+USE Geography
+SELECT PeakName FROM Peaks
+ORDER BY PeakName ASC
+
+--23.
+SELECT TOP(30) CountryName, [Population]
+FROM Countries
+WHERE ContinentCode = 'EU'
+ORDER BY Population DESC, CountryName
+
+SELECT * FROM Countries
+
+--24.
+SELECT CountryName, CountryCode,
+CASE
+	WHEN CurrencyCode = 'EUR' THEN 'Euro'
+	ELSE 'Not Euro'
+END AS Currency
+FROM Countries
+ORDER BY CountryName ASC
+
+--III - Part Three (Use Diablo database)
+USE Diablo
+--25.
+SELECT Name From Characters
+ORDER BY Name
+
