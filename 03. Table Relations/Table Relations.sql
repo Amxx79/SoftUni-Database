ecc350bb0ab806b5f53c2d2b089aa880603bb830
Amxx79 <e.siwanov@gmail.com>
2024-05-26 01:35:43 +0300

    Table Relations

---
+++ 03. Table Relations/Table Relations.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE TableRelations
+USE TableRelations
+
+--DROP TABLE Passports
+--DROP TABLE Persons
+--DROP DATABASE TableRelations
+
+--1.
+
+CREATE TABLE Passports
+(
+	PassportID INT PRIMARY KEY IDENTITY(101, 1),
+	PassportNumber VARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Persons
+(
+	PersonID INT PRIMARY KEY IDENTITY(1, 1),
+	FirstName VARCHAR(32) NOT NULL,
+	Salary DECIMAL(10, 2),
+	PassportID INT UNIQUE FOREIGN KEY REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports
+	VALUES
+	('N34FG21B'),
+	('K65LO4R7'),
+	('ZE657QP2')
+
+INSERT INTO Persons
+	VALUES
+	('Roberto', 43300, 102),
+	('Tom', 56100, 103),
+	('Yana', 60200, 101)
+
+--2.
+
+CREATE TABLE Manufacturers
+(
+	ManufacturerID INT PRIMARY KEY IDENTITY(1,1),
+	[Name] VARCHAR(32) NOT NULL,
+	EstablishedOn DATETIME2
+)
+
+CREATE TABLE Models
+(
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(32) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers
+	VALUES
+	('BMW', '07/03/1916'),
+	('Tesla', '01/01/2003'),
+	('Lada', '01/05/1966')
+
+INSERT INTO Models
+VALUES
+	('X1', 1),
+	('i6', 1),
+	('Model S', 2),
+	('Model X', 2),
+	('Model 3', 2),
+	('Nova', 3)
+
+SELECT * FROM Manufacturers
+
+--03.
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(16) NOT NULL
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(16) NOT NULL
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT FOREIGN KEY REFERENCES Exams(ExamID)
+	CONSTRAINT PK_StudentsExams PRIMARY KEY(StudentID, ExamID)
+)
+
+INSERT INTO Students
+	VALUES
+	('Mila'),
+	('Toni'),
+	('Ron')
+
+INSERT INTO Exams
+	VALUES
+	('SpringMVC'),
+	('Neo4j'),
+	('Oracle 11g')
+
+INSERT INTO StudentsExams
+	VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103)
+
+--JOIN Operator on the third table 
+
+SELECT Students.Name, Exams.Name
+FROM StudentsExams
+JOIN Students ON StudentsExams.StudentID = Students.StudentID
+JOIN Exams ON StudentsExams.ExamID = Exams.ExamID
