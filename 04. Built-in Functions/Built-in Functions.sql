2e52fde60be2ec25ff83865ae6cdbb8e9dcb5bfd
Amxx79 <e.siwanov@gmail.com>
2024-05-30 15:06:46 +0300

    Built-in Functions

--- 04. Built-in Functions/Built-in Functions.sql
+++ 04. Built-in Functions/Built-in Functions.sql
@@ -90,3 +90,97 @@ WHERE Result.Rank = 2
 	ORDER BY Salary DESC
 
 
+--12
+USE Geography
+
+SELECT CountryName, IsoCode
+FROM Countries
+WHERE CountryName LIKE '%A%A%A%'
+ORDER BY IsoCode
+
+--13
+SELECT PeakName, RiverName,
+LOWER(CONCAT(SUBSTRING(PeakName, 1, LEN(PeakName)-1), RiverName)) AS Mix
+FROM Peaks, Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
+
+--14
+USE Diablo
+
+SELECT TOP 50 [Name], FORMAT([Start], 'yyyy-MM-dd') AS Start
+FROM Games
+WHERE DATEPART(Year, Start) BETWEEN 2011 AND 2012
+ORDER BY [Start], [Name]
+
+SELECT UserName, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS 'Email Provider'
+FROM Users
+ORDER BY [Email Provider], [Username]
+
+--16
+
+--LEGEND: '_' is symbol we should see it, '%' is symbol that may be or bay not be in ip 
+SELECT Username, IpAddress 
+FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+
+
+--17
+SELECT [Name] AS Game,
+[Part of the Day] = 
+CASE
+	WHEN DATEPART(HOUR, Start) < 12 THEN 'Morning'
+	WHEN DATEPART(HOUR, Start) < 18 THEN 'Afternoon'
+	WHEN DATEPART(HOUR, Start) < 24
+	THEN 'Evening'
+	END,
+[Duration] = 
+CASE
+	WHEN Duration <= 3 THEN 'Extra Short'
+	WHEN Duration >= 4 AND Duration <= 6 THEN 'Short'
+	WHEN Duration >= 6 THEN 'Long'
+	WHEN Duration IS NULL THEN 'Extra Long'
+	END
+FROM Games
+ORDER BY [Name], [Duration], [Part of the Day]
+
+--18
+USE Orders
+
+SELECT ProductName, OrderDate,
+DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+FROM Orders
+
+--19
+CREATE DATABASE People
+
+USE People
+
+CREATE TABLE People
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(32),
+	Birthdate DATETIME2
+)
+
+INSERT INTO People
+	VALUES
+	('Victor', '2000-12-07 00:00:00.000'),
+	('Steven', '1992-09-10 00:00:00.000'),
+	('Stephen', '1910-09-19 00:00:00.000'),
+	('John', '2010-01-06 00:00:00.000'),
+	('Elenko', '2000-02-18 19:00:00.000')
+
+
+SELECT * FROM
+(
+	SELECT Name, 
+	[Age in Years] = DATEDIFF(YEAR, [Birthdate], GETDATE()),
+	DATEDIFF(MONTH, [Birthdate], GETDATE()) AS [Age in Months],
+	DATEDIFF(DAY, [Birthdate], GETDATE()) AS [Age in Days],
+	DATEDIFF(MINUTE, [Birthdate], GETDATE()) AS [Age in Minutes]
+	FROM People
+) AS Result
+
